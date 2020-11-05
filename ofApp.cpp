3380baa8c54060f2a32f3c5169ba954b3d43c098
scottnolan17 <54386095+scottnolan17@users.noreply.github.com>
2020-11-04 20:07:24 -0800

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,346 @@
+#include "ofApp.h"
+
+
+// Positive-only modulo, because the default fmod() can return negative results
+//
+float pos_mod(float n, float d) {
+	float result = fmod(n, d);
+	return result < 0.0 ? result + d : result;
+}
+
+// Return the color at the given point (in world space); either a flat diffuse color or part of a texture
+//
+ofColor Plane::getColorAt(glm::vec3 point) {
+	if (!hasTexture) return diffuseColor;
+	glm::vec3 relOrigin = position;		// relative origin, in other words, where the texture starts from
+	if (!bInfinite) {
+		relOrigin = relOrigin + (basis1 * height / 2) - (basis2 * width / 2);	// if it's a finite plane, start drawing from the corner
+	}
+	return texture.getColor(
+		pos_mod(floor(glm::dot( (point - relOrigin), (basis2*80) )), texture.getWidth()),
+		texture.getHeight() - 1 - pos_mod(floor(glm::dot( (point - relOrigin), (basis1*80) )), texture.getHeight())
+);
+	// ^ This looks like a complete mess but it's actually not too bad: 
+	//		basis*80 turns the basis from being 1 OF unit long, which is kind of big, into being 80 times smaller
+	//		point - relOrigin is the vector to the intersect point from the relative origin
+	//		glm::dot() is how you get subspace coordinates given a basis vector and a vector on the subspace (i.e. the location of the pixels on the image source file)
+	//		floor() so we don't accidentally get any overflow
+	//		pos_mod() so that the image repeats in a tile pattern
+	//		texture.getheight() - 1 so the image isn't upside down
+}
+
+// Intersect Ray with Plane  (wrapper on glm::intersect*
+//
+bool Plane::intersect(const Ray &ray, glm::vec3 & point, glm::vec3 & normalAtIntersect) {
+	float dist;
+	bool hit = glm::intersectRayPlane(ray.p, ray.d, position, this->normal, dist);
+	Ray r = ray;
+	glm::vec3 poi = r.evalPoint(dist);
+	if (!bInfinite) {		// only for finite planes
+		glm::vec3 relativePoi = poi - position;						// vector position relative to plane position
+		glm::vec3 b1Proj = glm::dot(relativePoi, basis1) * basis1;		// Projection of the relative point of intersection (poi) onto
+		glm::vec3 b2Proj = glm::dot(relativePoi, basis2) * basis2;		// the two bases of the plane.
+
+		hit = hit && glm::length(b1Proj) <= height / 2 && glm::length(b2Proj) <= width / 2;		// if either of these projections is too long, the poi must be off the finite plane
+	}
+	if (hit) {
+		point = poi;
+		normalAtIntersect = this->normal;
+	}
+	return (hit);
+}
+
+// Intersect Ray with Sphere 
+//
+bool Sphere::intersect(const Ray &ray, glm::vec3 & point, glm::vec3 & normalAtIntersect) {
+	float dist;
+	bool hit = glm::intersectRaySphere(ray.p, ray.d, position, glm::pow2(radius), dist);
+	if (hit) {
+		Ray r = ray;
+		point = r.evalPoint(dist);
+		normalAtIntersect = glm::normalize(point - position);
+	}
+	return (hit);
+}
+
+
+// Convert (u, v) to (x, y, z) 
+// We assume u,v is in [0, 1]
+//
+glm::vec3 ViewPlane::toWorld(float u, float v) {
+	float w = width();
+	float h = height();
+	return (glm::vec3((u * w) + min.x, (v * h) + min.y, position.z));
+}
+
+// Get a ray from the current camera position to the (u, v) position on
+// the ViewPlane
+//
+Ray RenderCam::getRay(float u, float v) {
+	glm::vec3 pointOnPlane = view.toWorld(u, v);
+	return(Ray(position, glm::normalize(pointOnPlane - position)));
+}
+
+// Draw the viewplane as a rectangle
+//
+void RenderCam::drawFrustum() {
+	view.draw();
+	Ray r1 = getRay(0, 0);
+	Ray r2 = getRay(0, 1);
+	Ray r3 = getRay(1, 1);
+	Ray r4 = getRay(1, 0);
+	float dist = glm::length((view.toWorld(0, 0) - position));
+	r1.draw(dist);
+	r2.draw(dist);
+	r3.draw(dist);
+	r4.draw(dist);
+}
+
+// Cast rays out from the camera's perspective to create an image output to a file called...
+//
+void ofApp::rayTrace() {
+	float u, v;
+	glm::vec3 intersectPt, normal;
+
+	for (int i = 0; i < imageWidth; i++) {
+		for (int j = 0; j < imageHeight; j++) {
+			u = (i + 0.5) / imageWidth;
+			v = (j + 0.5) / imageHeight;
+
+			Ray ray = renderCam.getRay(u, v);
+			float distance = numeric_limits<float>::infinity();
+			float closest = distance;
+			ofColor color = ofColor::darkGray;	// default to dark grey if no objects are hit by the ray
+
+			for (SceneObject *obj : scene) {
+				if (obj->intersect(ray, intersectPt, normal)) {
+					if (glm::distance(ray.p, intersectPt) < closest) {	// new closest object
+						closest = glm::distance(ray.p, intersectPt);
+						color = phong(intersectPt, normal, obj->getColorAt(intersectPt), obj->specularColor, phongPower);
+
+					}
+				}
+			}
+			image.setColor(i, imageHeight - j - 1, color);
+		}
+	}
+
+	image.update();
+
+	image.save("raytraced.png");
+	cout << "ray trace successful: output saved as bin/data/raytraced.png" << endl;
+	bShowImage = true;
+
+}
+
+// Apply a shiny Blinn-Phong shader effect to a color, given the point on the scene object, the normal, the unshaded color (diffuse), the highlight color (specular), and the strength of the effect
+//
+ofColor ofApp::phong(const glm::vec3 &p, const glm::vec3 &norm, const ofColor diffuse, const ofColor specular, float power) {	ofColor result = lambert(p, norm, diffuse);	for (Light *l : lights) {
+		if (!l->isBlocked(p + (norm * 0.01), scene)) {
+			result += specular * (l->intensity / lightFalloff) * glm::pow(max((float)0, glm::dot(norm, glm::normalize(l->position - p + renderCam.position - p))), power);
+		}
+	}	return result;}
+
+// Apply a matte Lambert shading effect to a color, given the point on the scene object, the normal, and the unshaded color (diffuse)
+//
+ofColor ofApp::lambert(const glm::vec3 &p, const glm::vec3 &norm, const ofColor diffuse) {
+	ofColor result = diffuse * (ambientStrength);	// ambient light level
+	for (Light *l : lights) {
+		if (!l->isBlocked(p + (norm * 0.01), scene)) {
+			result += diffuse * (l->intensity / lightFalloff) * max((float)0, glm::dot(norm, glm::normalize(l->position - p)));
+		}
+	}
+	return result;
+}
+
+// Checks if the line segment between the given point and the light is blocked by any of the given SceneObjects
+//
+bool Light::isBlocked(glm::vec3 surfacePoint, vector<SceneObject *> sceneObjs) {
+	glm::vec3 intersectPt, normal;
+	Ray ray = Ray(position, glm::normalize(surfacePoint-position));
+	for (SceneObject *obj : sceneObjs) {
+		if (obj->intersect(ray, intersectPt, normal) && glm::distance(intersectPt, position) < glm::distance(surfacePoint, position) ) return true; 
+	}
+	return false;
+}
+
+// Checks if the line segment between the given point and the spotlight is blocked by any of the given SceneObjects or is outside the light cone
+//
+bool Spotlight::isBlocked(glm::vec3 surfacePoint, vector<SceneObject *> sceneObjs) {
+	glm::vec3 intersectPt, normal;
+	Ray ray = Ray(position, glm::normalize(surfacePoint - position));
+	if (glm::angle(direction, glm::normalize(surfacePoint - position)) > glm::radians(angle)) return true;
+	for (SceneObject *obj : sceneObjs) {
+
+		if (obj->intersect(ray, intersectPt, normal) && glm::distance(intersectPt, position) < glm::distance(surfacePoint, position)) return true;
+	}
+	return false;
+}
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	image.allocate(imageWidth, imageHeight, OF_IMAGE_COLOR);
+	
+	mainCam.setDistance(30);
+	mainCam.lookAt(glm::vec3(0, 0, 0));
+	sideCam.setPosition(glm::vec3(renderCam.position.z * -1, renderCam.position.y, renderCam.position.x));
+	sideCam.lookAt(renderCam.aim);
+	previewCam.setPosition(renderCam.position);
+	previewCam.lookAt(renderCam.aim);
+
+	theCam = &mainCam;
+
+	ofSetBackgroundColor(ofColor::black);
+
+	Plane *floorPlane = new Plane(glm::vec3(0, -1, 0), glm::vec3(0.05, 1, 0.1), ofColor::lightBlue);
+	floorPlane->setTexture(ofImage("07_wood grain PBR texture _seamless/07_wood grain PBR texture.jpg"));
+	Plane *backdropPlane = new Plane(glm::vec3(0, 5, -32), glm::vec3(0, 0, 1), ofColor::grey);
+	backdropPlane->setTexture(ofImage("26_white bricks texture-seamless.jpg"));
+	//Plane *picturePlane = new Plane(glm::vec3(-7, 4.5, -12), glm::vec3(0.6, 0.2, 1), ofColor::grey);
+	//picturePlane->width = 7.6;
+	//picturePlane->height = 4.8;
+	//picturePlane->bInfinite = false;
+	//picturePlane->setTexture(ofImage("persistenceofmemory1931.jpg"));
+
+	scene.push_back(floorPlane);
+	//scene.push_back(picturePlane);
+	scene.push_back(backdropPlane);
+	//scene.push_back(new Plane(glm::vec3(0, -1, 0), glm::vec3(0, 1, 0), ofColor::lightBlue));	// openframeworks seems to have implemented
+	scene.push_back(new Sphere(glm::vec3(3, 1.4, -2), 2.5, ofColor::chartreuse));					// a million different colors; might as well
+	scene.push_back(new Sphere(glm::vec3(-3.5, 1.7, -4), 2, ofColor::darkMagenta));				// use some of the fun ones...
+	scene.push_back(new Sphere(glm::vec3(0, 2, -8), 2.2, ofColor::orangeRed));
+	//scene.push_back(new Sphere(glm::vec3(-7, 4, -7), 1.5, ofColor::ivory));
+	//scene.push_back(new Sphere(glm::vec3(130, 70, -245), 8, ofColor::coral));
+	//scene.push_back(new Sphere(glm::vec3(0, 1.5, 0), 2.5, ofColor::chartreuse));					
+
+
+	//lights.push_back(new Light(glm::vec3(-10, 8, 10), 1.0));
+	lights.push_back(new Light(glm::vec3(10, 4.5, 12), 1.0));
+	//lights.push_back(new Light(glm::vec3(-10, 3, -6), 1.0));
+	lights.push_back(new Spotlight(glm::vec3(-10, 12, 10), glm::vec3(10, -9, -10), 1.5, 20.0));	// pointed at the origin
+	//lights.push_back(new Spotlight(glm::vec3(4, 7, 10), glm::vec3(-6, -10, -12), 1.0, 20.0));
+	//lights.push_back(new Spotlight(glm::vec3(-8, 3, 10), glm::vec3(8, -2, -10), 1.0, 25.0));
+	//lights.push_back(new Spotlight(glm::vec3(0, 15, 3), glm::vec3(0.2, -3, -1.4), 1.1, 20.0));
+	//lights.push_back(new Spotlight(glm::vec3(-4, 0, 8), glm::vec3(5, 1, -8), 1.0, 20.0));
+	//lights.push_back(new Spotlight(glm::vec3(4, 0, 8), glm::vec3(-5, 1, -8), 1.0, 20.0));
+	for (Light *l: lights) scene.push_back(l);
+
+	gui.setup();
+	gui.add(lightIntensity.setup("Light Intensity", 1.0, 0.0, 20.0));
+	gui.add(lightFalloff.setup("Light Falloff", 1.0, 1.0, 10.0));
+	gui.add(phongPower.setup("Phong Power", 100, 1, 400));
+	gui.add(ambientStrength.setup("Ambient Light Level", 0.3, 0.0, 1.0));
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	theCam->begin();
+
+	drawAxis(glm::vec3(0, 0, 0));
+	ofSetColor(ofColor::white);
+	for (SceneObject *obj : scene) {
+		obj->draw();
+	}
+	ofDrawSphere(renderCam.position, 0.5);
+	renderCam.drawFrustum();
+
+	theCam->end();
+
+	if (bShowImage) image.draw(glm::vec3(0, 0, 0), ofGetWindowHeight() / 4 * renderCam.view.getAspect(), ofGetWindowHeight() / 4);		// do a sort of picture-in-picture effect to display the rendered image
+	else gui.draw();
+}
+
+void ofApp::drawAxis(glm::vec3 pos) {
+
+	// X axis
+	ofSetColor(ofColor::red);
+	ofDrawLine(pos, glm::vec3(pos.x + 5, pos.y, pos.z));
+
+	// Y axis
+	ofSetColor(ofColor::green);
+	ofDrawLine(pos, glm::vec3(pos.x, pos.y + 5, pos.z));
+
+	// Z axis
+	ofSetColor(ofColor::blue);
+	ofDrawLine(pos, glm::vec3(pos.x, pos.y, pos.z + 5));
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	switch (key) {
+	case '1':		// switch to front cam
+		theCam = &previewCam;
+		break;
+	case '2':		// switch to side cam
+		theCam = &sideCam;
+		break;
+	case '3':		// switch to easycam
+		theCam = &mainCam;
+		break;
+	case 'r':		// render image
+		for (Light *l : lights) l->intensity = lightIntensity;
+		rayTrace();
+		break;
+	case ' ':		// toggle image overlay
+		bShowImage = !bShowImage;
+		break;
+
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
+
---
+++ ofApp.h
@@ -0,0 +1,299 @@
+//
+//  SJSU CS 116A F2020
+//  Ray tracer using Lambert and Blinn-Phong shading
+//  illuminated by fixed light sources to render output
+//  image files. Currently only geometric shapes.
+//
+//  - Scott Nolan 2020
+//
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "glm/gtx/intersect.hpp"
+
+//  General Purpose Ray class 
+//
+class Ray {
+public:
+	Ray(glm::vec3 p, glm::vec3 d) { this->p = p; this->d = d; }
+	void draw(float t) { ofDrawLine(p, p + t * d); }
+
+	glm::vec3 evalPoint(float t) {
+		return (p + t * d);
+	}
+
+	glm::vec3 p, d;
+};
+
+//  Base class for any renderable object in the scene
+//
+class SceneObject {
+public: 
+	virtual void draw() = 0;    // pure virtual funcs - must be overloaded
+	virtual bool intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal) { cout << "SceneObject::intersect" << endl; return false; }
+	virtual ofColor getColorAt(glm::vec3 point) { return diffuseColor; }
+	
+	// any data common to all scene objects goes here
+	glm::vec3 position = glm::vec3(0, 0, 0);
+
+	// material properties (we will ultimately replace this with a Material class - TBD)
+	//
+	ofColor diffuseColor = ofColor::grey;    // default colors - can be changed.
+	ofColor specularColor = ofColor::lightGray;
+};
+
+//  A light source to light the scene
+//
+class Light : public SceneObject {
+public:
+	Light(glm::vec3 pos, float brightness) { position = pos; intensity = brightness; }
+	Light() { position = glm::vec3(0, 5, 0); intensity = 1.0; }
+
+	void draw() { 
+		ofSetColor(ofColor::lightYellow);
+		ofDrawSphere(position, 0.3);  
+		ofSetColor(ofColor::white);
+	}
+	bool intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal) { return false; }  // don't want to include the light sources in a rendering
+
+	virtual bool isBlocked(glm::vec3, vector<SceneObject *>);
+
+	float intensity = 1.0;
+};
+
+//	A directional light source that projects its light in a cone
+//
+class Spotlight : public Light {
+public:
+	Spotlight(glm::vec3 pos, glm::vec3 dir, float brightness, float coneAngle) { position = pos; direction = glm::normalize(dir); intensity = brightness; angle = coneAngle; }
+	Spotlight() { position = glm::vec3(0, 5, 0); direction = glm::vec3(0, -1, 0), intensity = 1.0; angle = 45;  }
+
+	void draw() {
+		ofSetColor(ofColor::goldenRod);
+		ofDrawSphere(position, 0.3);
+		ofDrawArrow(position, position + (direction * 0.6), 0.2);
+		ofSetColor(ofColor::white);
+	}
+	
+	void setDirection(glm::vec3 newDir) { direction = glm::normalize(newDir); }
+
+	bool isBlocked(glm::vec3, vector<SceneObject *>);
+
+	glm::vec3 direction;
+	float angle;
+};
+
+//  General purpose sphere  (assume parametric)
+//
+class Sphere: public SceneObject {
+public:
+	Sphere(glm::vec3 p, float r, ofColor diffuse = ofColor::lightGray) { position = p; radius = r; diffuseColor = diffuse; }
+	Sphere() {}
+	bool intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal); //{
+	//	return (glm::intersectRaySphere(ray.p, ray.d, position, radius, point, normal));
+	//}
+	void draw()  { 
+		ofDrawSphere(position, radius); 
+	}
+
+	float radius = 1.0;
+};
+
+//  Mesh class (will complete later- this will be a refinement of Mesh from Project 1)
+//
+class Mesh : public SceneObject {
+	bool intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal) { return false;  }
+	void draw() { }
+};
+
+
+//  General purpose plane 
+//
+class Plane: public SceneObject {
+public:
+	Plane(glm::vec3 p, glm::vec3 n, ofColor diffuse = ofColor::darkOliveGreen, float w = 20, float h = 20 ) {
+		position = p; 
+		setNormal(n);
+		width = w;
+		height = h;
+		diffuseColor = diffuse;
+		plane.rotateDeg(90, 1, 0, 0);
+	}
+	Plane() { }
+	bool intersect(const Ray &ray, glm::vec3 & point, glm::vec3 & normal);
+	void draw() {
+		plane.setPosition(position);
+		plane.setWidth(width);
+		plane.setHeight(height);
+		plane.setResolution(4, 4);
+		plane.drawWireframe();
+
+		ofSetColor(ofColor::red);
+		ofDrawArrow(position, position + normal);
+		ofSetColor(ofColor::white);
+		ofDrawArrow(position, position + basis1);
+		ofDrawArrow(position, position + basis2);
+	}
+	void setTexture(ofImage image) {
+		texture = image;
+		hasTexture = true;
+	}
+	ofImage getTexture() {
+		return texture;
+	}
+	void setNormal(glm::vec3 norm) {
+		normal = glm::normalize(norm);
+
+		// create a pair of vectors linearly independent from the normal and each other...
+		glm::vec3 x1 = glm::normalize(glm::vec3(-normal.y, normal.x, 0));	
+		if (glm::length(glm::vec3(-normal.y, normal.x, 0)) == 0) x1 = glm::normalize(glm::vec3(0, -normal.z, normal.y));
+		glm::vec3 x2 = glm::normalize(glm::vec3(normal.z, 0, -normal.x));
+		if (glm::length(glm::vec3(normal.z, 0, -normal.x)) == 0) x2 = glm::normalize(glm::vec3(0, -normal.z, normal.y));
+
+		// Use Gram-Schmidt to make them into orthogonal bases. Do this once and store it in the plane's structure rather than every time a ray is cast.
+		basis1 = x1 - (glm::dot(x1, normal) * normal);
+		basis2 = x2 - (glm::dot(x2, normal) * normal) - (glm::dot(x2, basis1) * basis1);
+		basis1 = glm::normalize(basis1);
+		basis2 = glm::normalize(basis2);
+
+
+	}
+	glm::vec3 getNormal() {
+		return normal;
+	}
+
+	ofColor getColorAt(glm::vec3);
+
+	ofPlanePrimitive plane;
+	float width = 20;
+	float height = 20;
+	bool bInfinite = true;
+
+private:
+	bool hasTexture = false;	// no texture by default
+	ofImage texture;
+
+	glm::vec3 normal = glm::vec3(0, 1, 0);
+	glm::vec3 basis1 = glm::vec3(1, 0, 0);
+	glm::vec3 basis2 = glm::vec3(0, 0, 1);
+};
+
+// view plane for render camera
+// 
+class  ViewPlane: public Plane {
+public:
+	ViewPlane(glm::vec2 p0, glm::vec2 p1) { min = p0; max = p1; }
+
+	ViewPlane() {                         // create reasonable defaults (6x4 aspect)
+		min = glm::vec2(-3, -2);
+		max = glm::vec2(3, 2);
+		position = glm::vec3(0, 0, 5);
+		setNormal(glm::vec3(0, 0, 1));      // viewplane currently limited to Z axis orientation
+	}
+
+	void setSize(glm::vec2 min, glm::vec2 max) { this->min = min; this->max = max; }
+	float getAspect() { return width() / height(); }
+
+	glm::vec3 toWorld(float u, float v);   //   (u, v) --> (x, y, z) [ world space ]
+
+	void draw() {
+		ofDrawRectangle(glm::vec3(min.x, min.y, position.z), width(), height());
+	}
+
+	
+	float width() {
+		return (max.x - min.x);
+	}
+	float height() {
+		return (max.y - min.y); 
+	}
+
+	// some convenience methods for returning the corners
+	//
+	glm::vec2 topLeft() { return glm::vec2(min.x, max.y); }
+	glm::vec2 topRight() { return max; }
+	glm::vec2 bottomLeft() { return min;  }
+	glm::vec2 bottomRight() { return glm::vec2(max.x, min.y); }
+
+	//  To define an infinite plane, we just need a point and normal.
+	//  The ViewPlane is a finite plane so we need to define the boundaries.
+	//  We will define this in terms of min, max  in 2D.  
+	//  (in local 2D space of the plane)
+	//  ultimately, will want to locate the ViewPlane with RenderCam anywhere
+	//  in the scene, so it is easier to define the View rectangle in a local'
+	//  coordinate system.
+	//
+	glm::vec2 min, max;
+};
+
+
+//  render camera  - currently must be z axis aligned (we will improve this in project 4)
+//
+class RenderCam: public SceneObject {
+public:
+	RenderCam() {
+		position = glm::vec3(0, 0, 10);
+		aim = glm::vec3(0, 0, -1);
+	}
+	Ray getRay(float u, float v);
+	void draw() { ofDrawBox(position, 1.0); };
+	void drawFrustum();
+
+	glm::vec3 aim;
+	ViewPlane view;          // The camera viewplane, this is the view that we will render 
+};
+
+  
+
+class ofApp : public ofBaseApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void rayTrace();
+		void drawGrid() { ofDrawGrid(); }
+		void drawAxis(glm::vec3);
+		ofColor lambert(const glm::vec3&, const glm::vec3&, ofColor);
+		ofColor phong(const glm::vec3&, const glm::vec3&, const ofColor, const ofColor, float);
+
+		bool bHide = true;
+		bool bShowImage = false;
+
+		ofEasyCam  mainCam;
+		ofCamera sideCam;
+		ofCamera previewCam;
+		ofCamera  *theCam;    // set to current camera either mainCam or sideCam
+
+		// set up one render camera to render image throughn
+		//
+		RenderCam renderCam;
+		ofImage image;
+
+		vector<SceneObject *> scene;
+		vector<Light *> lights;
+
+		ofxFloatSlider lightIntensity;
+		ofxFloatSlider lightFalloff;
+		ofxFloatSlider phongPower;
+		ofxFloatSlider ambientStrength;
+		ofxPanel gui;
+
+		int imageWidth = 1200;
+		int imageHeight = 800;
+};
+ 
